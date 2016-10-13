e228d466f35ada535250a34e5113607e2cf86061
asandriss <fedorhajdu@gmail.com>
2016-10-13 09:07:53 +0400

    Make PS history permanent

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,6 +2,7 @@
 
 Start-SshAgent
 
+
 # Load posh-git example profile
 . 'C:\Users\fedor.hajdu\Documents\Projects\posh-git\profile.example.ps1'
 
@@ -18,3 +19,12 @@ function sln($path = '.', $fileName = '') {
         write-host 'Solution file was not found' -foreground "yellow"
     }
 }
+
+# Make PS history permanent in order this to work need PSGet, PSUrl and PSGetLine installed
+
+$HistoryFilePath = Join-Path ([Environment]::GetFolderPath('UserProfile')) .ps_history
+Register-EngineEvent PowerShell.Exiting -Action { Get-History | Export-Clixml $HistoryFilePath } | out-null
+if (Test-path $HistoryFilePath) { Import-Clixml $HistoryFilePath | Add-History }
+# if you don't already have this configured...
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
