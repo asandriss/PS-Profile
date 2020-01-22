57742554dc735cc3f9e684d5f0f12f76ce61a237
asandriss <fedorhajdu@gmail.com>
2020-01-22 18:41:20 +0400

    Update Microsoft.PowerShell_profile.ps1
    Add vcvarsall.bat and invoke-batchfile method

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,11 @@
-# Import-Module -Name posh-git 
+Import-Module -Name posh-git
 
 Start-SshAgent
 
-
-# Load posh-git example profile
-. 'C:\Users\fedor.hajdu\Documents\Projects\posh-git\profile.example.ps1'
+function Test-Administrator {
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
 
 function sln($path = '.', $fileName = '') {
     if($fileName) {
@@ -28,3 +29,33 @@ if (Test-path $HistoryFilePath) { Import-Clixml $HistoryFilePath | Add-History }
 # if you don't already have this configured...
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+# Init VC build tools
+# & "c:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
+
+function Invoke-BatchFile
+{
+    param([string]$Path, [string]$Parameters)
+
+    $tempFile = [IO.Path]::GetTempFileName()
+
+    ## Store the output of cmd.exe.  We also ask cmd.exe to output
+    ## the environment table after the batch file completes
+    cmd.exe /c " `"$Path`" $Parameters && set " > $tempFile
+
+    ## Go through the environment variables in the temp file.
+    ## For each of them, set the variable in our local environment.
+    Get-Content $tempFile | Foreach-Object {
+        if ($_ -match "^(.*?)=(.*)$") {
+            Set-Content "env:\$($matches[1])" $matches[2]
+        }
+        else {
+            $_
+        }
+    }
+
+    Remove-Item $tempFile
+}
+
+$VcVars = 'c:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat'
+Invoke-BatchFile $VcVars x64
